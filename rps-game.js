036bf0b758067d3977f0a0fdc41598a9a2f66c56
a-bills <121698448+a-bills@users.noreply.github.com>
2023-02-28 23:18:59 -0500

    Add score keeping system

--- rps-game.js
+++ rps-game.js
@@ -13,43 +13,51 @@ function getComputerChoice () {
 
 function playRound (playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return `Both chose ${playerSelection}! It's a Tie.`;
+        return [`Both chose ${playerSelection}! It's a Tie.`, null];
     } else if (computerSelection === 'rock') {
         switch (playerSelection) {
             case 'paper': 
-                return 'Paper covers Rock! You won this round.';
+                return ['Paper covers Rock! You won this round.', true];
                 break;
             case 'scissors': 
-                return 'Scissors smashed by Rock! You lost this round.'; 
+                return ['Scissors smashed by Rock! You lost this round.', false]; 
                 break;
         }
     } else if (computerSelection === 'paper') {
         switch (playerSelection) {
             case 'scissors': 
-                return 'Scissors cuts Paper! You won this round.'; 
+                return ['Scissors cuts Paper! You won this round.', true]; 
                 break;
             case 'rock': 
-                return 'Rock covered by Paper! You lost this round.';
+                return ['Rock covered by Paper! You lost this round.', false];
                 break;
         }
     } else if (computerSelection === 'scissors') {
         switch (playerSelection) {
             case 'rock': 
-                return 'Rock smashes Scissors! You won this round.'; 
+                return ['Rock smashes Scissors! You won this round.', true]; 
                  break;
             case 'paper': 
-                return 'Paper cut by Scissors! You lost this round.';
+                return ['Paper cut by Scissors! You lost this round.', false];
                  break;
             }
     } 
 }
 
 function game() {
-    for (let i = 0; i < 5; i++) {
+    let playerScore = 0;
+    let computerScore = 0;
+    while (playerScore < 3 && computerScore < 3) {
         let playerSelection = prompt("Choose your fighter: Rock, Paper, or Scissors?").toLowerCase();
         let computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
+        let roundResults = playRound(playerSelection, computerSelection)[0];
+        let roundWon = playRound(playerSelection, computerSelection)[1];
+        if (roundWon !== null) {
+            roundWon ? playerScore += 1 : computerScore += 1;
+        }  
+        console.log(roundResults, `\nYour score: ${playerScore}\nOpponent's Score: ${computerScore}`);
     }
+    console.log(playerScore > computerScore ? "You Won!" : "You Lost!")
 }
 
 game();
