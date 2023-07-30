6890e971c8034bbfc02c973db30176f3fc3e8856
a-bills <121698448+a-bills@users.noreply.github.com>
2023-07-30 19:30:38 -0400

    Add updateScore function

--- rps-game.js
+++ rps-game.js
@@ -1,5 +1,34 @@
-const div = document.querySelector('div');
+const results = document.querySelector('.results');
+const score = document.querySelector('.score');
+let playerScore = 0;
+let computerScore = 0;
 
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    let playerSelection = button.innerText;
+    button.addEventListener('click', () => playRound(playerSelection));
+});
+
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
+    if (playerSelection === computerSelection) {
+        results.textContent = `You both chose ${playerSelection}! It's a Tie.`;
+    } 
+    else if ((playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+    (playerSelection === 'Paper' && computerSelection === 'Rock') ||
+    (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
+        results.textContent = (`You chose ${playerSelection} and your opponent chose ${computerSelection}.
+        ${playerSelection} beats ${computerSelection}! You won this round.`);
+        updateScore('player won');
+    } 
+    else if ((playerSelection === 'Rock' && computerSelection === 'Paper') ||
+    (playerSelection === 'Paper' && computerSelection === 'Scissors') ||
+    (playerSelection === 'Scissors' && computerSelection === 'Rock')) {
+        results.textContent =  (`You chose ${playerSelection} and your opponent chose ${computerSelection}.
+        ${playerSelection} is beaten by ${computerSelection}! You lost this round.`);
+        updateScore('computer won');
+    }
+}
 
 function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3);
@@ -16,25 +45,34 @@ function getComputerChoice() {
     }
 }
 
-function playRound(playerSelection) {
-    let computerSelection = getComputerChoice();
-    if (playerSelection === computerSelection) {
-        //Update score
-        div.textContent = `You both chose ${playerSelection}! It's a Tie.`;
+function updateScore (roundResult) {
+    if (roundResult === 'player won') {
+        playerScore++;
+        score.textContent = (
+            `Your Score: ${playerScore}
+            Opponent's Score: ${computerScore}`
+        );
+        if (playerScore === 5) {
+            score.textContent = (
+                `Score limit reached! You won the game!
+                Your Final Score: ${playerScore}
+                Opponent's Final Score: ${computerScore}`
+            );
+        }
     } 
-    if ((playerSelection === 'Rock' && computerSelection === 'Scissors') ||
-    (playerSelection === 'Paper' && computerSelection === 'Rock') ||
-    (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
-        //Update score
-        div.textContent = (`You chose ${playerSelection} and your opponent chose ${computerSelection}.
-        ${playerSelection} beats ${computerSelection}! You won this round.`);
-    } 
-    if ((playerSelection === 'Rock' && computerSelection === 'Paper') ||
-    (playerSelection === 'Paper' && computerSelection === 'Scissors') ||
-    (playerSelection === 'Scissors' && computerSelection === 'Rock')) {
-        //Update score
-        div.textContent =  (`You chose ${playerSelection} and your opponent chose ${computerSelection}.
-        ${playerSelection} is beaten by ${computerSelection}! You lost this round.`);
+    else if (roundResult === 'computer won') {
+        computerScore++;
+        score.textContent = (
+            `Your Score: ${playerScore}
+            Opponent's Score: ${computerScore}`
+        );
+        if (computerScore === 5) {
+            score.textContent = (
+                `Score limit reached! You lost the game.
+                Your Final Score: ${playerScore}
+                Opponent's Final Score: ${computerScore}`
+            );
+        }
     }
 }
 /*
@@ -55,8 +93,3 @@ console.groupEnd(`Round ${roundCounter}`);
 console.log(playerScore > computerScore ? "Score Limit Reached! You Won!" : "Score Limit Reached! You Lost!")
 */
 
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
-    let playerSelection = button.innerText;
-    button.addEventListener('click', () => playRound(playerSelection));
-});
