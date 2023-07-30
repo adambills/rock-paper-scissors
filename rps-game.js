10a1b2a35a8081d921dadac7a71d627515467499
a-bills <121698448+a-bills@users.noreply.github.com>
2023-07-30 17:57:35 -0400

    Remove the logic that plays until a score limit of 3

--- rps-game.js
+++ rps-game.js
@@ -1,4 +1,4 @@
-function getComputerChoice () {
+function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3);
     switch (randomNum) {
         case 0:
@@ -7,66 +7,56 @@ function getComputerChoice () {
         case 1:
             return 'paper';
             break;
-        case 2: 
+        case 2:
             return 'scissors';
             break;
-    } 
+    }
 }
 
-function playRound (playerSelection, computerSelection) {
-    let playerResult;
+function playRound(playerSelection, computerSelection) {
+    let playerResult = false;
     if (playerSelection === computerSelection) {
         playerResult = null;
         return [`Both chose ${playerSelection}! It's a Tie.`, playerResult];
     } else if (computerSelection === 'rock') {
         switch (playerSelection) {
             case 'paper':
-                playerResult = true; 
+                playerResult = true;
                 return ['Paper covers Rock! You won this round.', playerResult];
-            case 'scissors': 
-                return ['Scissors smashed by Rock! You lost this round.', playerResult]; 
+            case 'scissors':
+                return ['Scissors smashed by Rock! You lost this round.', playerResult];
         }
     } else if (computerSelection === 'paper') {
         switch (playerSelection) {
-            case 'scissors': 
+            case 'scissors':
                 playerResult = true;
-                return ['Scissors cuts Paper! You won this round.', playerResult]; 
-            case 'rock': 
+                return ['Scissors cuts Paper! You won this round.', playerResult];
+            case 'rock':
                 return ['Rock covered by Paper! You lost this round.', playerResult];
         }
     } else if (computerSelection === 'scissors') {
         switch (playerSelection) {
-            case 'rock': 
+            case 'rock':
                 playerResult = true;
-                return ['Rock smashes Scissors! You won this round.', playerResult]; 
-            case 'paper': 
+                return ['Rock smashes Scissors! You won this round.', playerResult];
+            case 'paper':
                 return ['Paper cut by Scissors! You lost this round.', playerResult];
-            }
-    } 
-}
-
-function playGame() {
-    console.log("Rock, Paper, Scissors! Best 3 out of 5.")
-    let playerScore = 0;
-    let computerScore = 0;
-    let roundCounter = 0;
-    while (playerScore < 3 && computerScore < 3) {
-        roundCounter++;
-        let playerSelection = prompt("Choose your fighter: Rock, Paper, or Scissors?").toLowerCase();
-        let computerSelection = getComputerChoice();
-        let resultMessage = playRound(playerSelection, computerSelection)[0];
-        let playerResult = playRound(playerSelection, computerSelection)[1];
-        if (playerResult !== null) {
-            playerResult ? playerScore++ : computerScore++;
         }
-        console.group(`Round ${roundCounter}`);
-        console.log(`You chose ${playerSelection} and your opponent chose ${computerSelection}.`)
-        console.log(resultMessage);
-        console.log(`Your score: ${playerScore}`);
-        console.log(`Opponent's Score: ${computerScore}`);
-        console.groupEnd(`Round ${roundCounter}`);
     }
-    console.log(playerScore > computerScore ? "Score Limit Reached! You Won!" : "Score Limit Reached! You Lost!")
 }
 
-playGame();
+let playerScore = 0;
+let computerScore = 0;
+let roundCounter = 0;
+roundCounter++;
+let playerSelection = prompt("Choose your fighter: Rock, Paper, or Scissors?").toLowerCase();
+let computerSelection = getComputerChoice();
+let resultMessage = playRound(playerSelection, computerSelection)[0];
+let playerResult = playRound(playerSelection, computerSelection)[1];
+console.group(`Round ${roundCounter}`);
+console.log(`You chose ${playerSelection} and your opponent chose ${computerSelection}.`)
+console.log(resultMessage);
+console.log(`Your score: ${playerScore}`);
+console.log(`Opponent's Score: ${computerScore}`);
+console.groupEnd(`Round ${roundCounter}`);
+console.log(playerScore > computerScore ? "Score Limit Reached! You Won!" : "Score Limit Reached! You Lost!")
