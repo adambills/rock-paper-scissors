afb3c057d473cadbe5360fe3b3e2160aadd940c0
a-bills <121698448+a-bills@users.noreply.github.com>
2023-07-30 19:49:23 -0400

    Add disableButtons function

--- rps-game.js
+++ rps-game.js
@@ -12,18 +12,23 @@ buttons.forEach((button) => {
 function playRound(playerSelection) {
     let computerSelection = getComputerChoice();
     if (playerSelection === computerSelection) {
+
         results.textContent = `You both chose ${playerSelection}! It's a Tie.`;
     } 
+
     else if ((playerSelection === 'Rock' && computerSelection === 'Scissors') ||
     (playerSelection === 'Paper' && computerSelection === 'Rock') ||
     (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
+
         results.textContent = (`You chose ${playerSelection} and your opponent chose ${computerSelection}.
         ${playerSelection} beats ${computerSelection}! You won this round.`);
         updateScore('player won');
     } 
+
     else if ((playerSelection === 'Rock' && computerSelection === 'Paper') ||
     (playerSelection === 'Paper' && computerSelection === 'Scissors') ||
     (playerSelection === 'Scissors' && computerSelection === 'Rock')) {
+
         results.textContent =  (`You chose ${playerSelection} and your opponent chose ${computerSelection}.
         ${playerSelection} is beaten by ${computerSelection}! You lost this round.`);
         updateScore('computer won');
@@ -58,6 +63,7 @@ function updateScore (roundResult) {
                 Your Final Score: ${playerScore}
                 Opponent's Final Score: ${computerScore}`
             );
+            disableButtons();
         }
     } 
     else if (roundResult === 'computer won') {
@@ -72,24 +78,15 @@ function updateScore (roundResult) {
                 Your Final Score: ${playerScore}
                 Opponent's Final Score: ${computerScore}`
             );
+            disableButtons();
         }
     }
 }
-/*
-let playerScore = 0;
-let computerScore = 0;
-let roundCounter = 0;
-roundCounter++;
-let playerSelection = prompt("Choose your fighter: Rock, Paper, or Scissors?").toLowerCase();
-let computerSelection = getComputerChoice();
-let resultMessage = playRound(playerSelection, computerSelection)[0];
-let playerResult = playRound(playerSelection, computerSelection)[1];
-console.group(`Round ${roundCounter}`);
-console.log(`You chose ${playerSelection} and your opponent chose ${computerSelection}.`)
-console.log(resultMessage);
-console.log(`Your score: ${playerScore}`);
-console.log(`Opponent's Score: ${computerScore}`);
-console.groupEnd(`Round ${roundCounter}`);
-console.log(playerScore > computerScore ? "Score Limit Reached! You Won!" : "Score Limit Reached! You Lost!")
-*/
+
+function disableButtons() {
+    buttons.forEach(button => {
+        button.disabled = true;
+    });
+}
+
 
